5386d5f4d9a3ff38f491e330faeddaf6a1bf6919
Jalu <jalunugroho118@gmail.com>
2026-03-30 21:20:03 +0700

    style(format): formatting file

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
-import js from '@eslint/js';
+import js from "@eslint/js";
 import pluginQuery from "@tanstack/eslint-plugin-query";
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ['dist', '.eslintrc.cjs', 'vite.config.ts']
+    ignores: ["dist", ".eslintrc.cjs", "vite.config.ts"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: {
@@ -22,23 +22,26 @@ export default [
       },
       parser: tseslint.parser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.json'],
+        project: ["./tsconfig.node.json", "./tsconfig.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
     },
   },
-  ...pluginQuery.configs["flat/recommended"]
+  ...pluginQuery.configs["flat/recommended"],
 ];
