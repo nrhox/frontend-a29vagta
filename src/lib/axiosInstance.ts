2b477cc61b362d73357dc6c286c3fc4b437b8a0f
Jalu <jalunugroho118@gmail.com>
2026-04-08 22:06:42 +0700

    feat(request-token): add feature generate token to request api

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,8 +1,48 @@
 import axios from "axios";
+import { encodeFixedToken, generateDynamicToken } from "./token";
 
-export const axiosInstance = axios.create({
+let cachedIP: string | null = sessionStorage.getItem("client_ip");
+
+export const initClientIP = async () => {
+  if (!cachedIP) {
+    try {
+      const res = await axios.get("https://api.ipify.org?format=json");
+      cachedIP = res.data.ip;
+      sessionStorage.setItem("client_ip", cachedIP!);
+    } catch {
+      cachedIP = "0.0.0.0";
+    }
+  }
+  return cachedIP;
+};
+
+const axiosInstance = axios.create({
   baseURL: import.meta.env.PROD
-    ? (import.meta.env.BACKEND_URL as string | undefined) ||
+    ? (import.meta.env.VITE_BACKEND_URL as string | undefined) ||
       "http://localhost:3030"
     : "http://localhost:3030",
 });
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    const userIP = cachedIP || (await initClientIP());
+    const tokenIp = encodeFixedToken(
+      userIP ?? "",
+      (import.meta.env.VITE_SECRET_KEY_SALT as string | undefined) ??
+        "tukang bakso",
+    );
+
+    const tokenKey = await generateDynamicToken(
+      (import.meta.env.VITE_SECRET_KEY_SALT as string | undefined) ??
+        "tukang bakso",
+    );
+
+    config.headers["x-api-token"] = tokenKey;
+    config.headers["x-entropy"] = tokenIp;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+export default axiosInstance;
